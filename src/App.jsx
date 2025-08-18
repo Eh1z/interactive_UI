46a7a9cd7100788f24261872c21916a74f3e7f82
Eh1z <godsentehiz11@gmail.com>
2025-08-19 00:04:59 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
 import React from "react";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className="w-full flex flex-col items-center px-4 md:px-16 lg:px-32 ">
+			<Navbar />
+			<main>
+				<ProductGallery />
+				<ProductInfo />
+			</main>
+		</div>
+	);
 };
 
 export default App;
+// Navbar
+const Navbar = () => {
+	const navLinks = [
+		{
+			name: "Collections",
+			url: "/collections",
+		},
+		{
+			name: "Men",
+			url: "/men",
+		},
+		{
+			name: "Women",
+			url: "/women",
+		},
+		{
+			name: "About",
+			url: "/about",
+		},
+		{
+			name: "Contact",
+			url: "/contact",
+		},
+	];
+
+	return (
+		<div className="flex justify-between w-full py-4">
+			<div className="w-[90px] md:w-[120px]">
+				<img src="/images/logo.svg" alt="logo" className="w-full " />
+			</div>
+
+			<div>links</div>
+			<div>cart</div>
+		</div>
+	);
+};
+
+// ProductGallery
+const ProductGallery = () => {
+	return <div>ProductGallery</div>;
+};
+// ProductInfo
+const ProductInfo = () => {
+	return <div>ProductInfo</div>;
+};
+// Cart
+const Cart = () => {
+	return <div>Cart</div>;
+};
