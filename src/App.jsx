53a98681fe9929e51d3adf3d07dee85b5cdb5b99
Eh1z <godsentehiz11@gmail.com>
2025-08-20 21:49:43 +0100

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { LuShoppingCart } from "react-icons/lu";
 const App = () => {
 	return (
 		<div className="w-full flex flex-col items-center px-4 md:px-16 lg:px-32 ">
@@ -39,13 +39,26 @@ const Navbar = () => {
 	];
 
 	return (
-		<div className="flex justify-between w-full py-4">
+		<div className="flex items-center justify-between w-full  border-b border-gray-200">
 			<div className="w-[90px] md:w-[120px]">
 				<img src="/images/logo.svg" alt="logo" className="w-full " />
 			</div>
 
-			<div>links</div>
-			<div>cart</div>
+			<div className="flex items-center h-full">
+				{navLinks.map((item, index) => (
+					<a
+						key={index}
+						href={item.url}
+						className="group px-4 py-4 text-gray-400   cursor-pointer"
+					>
+						{item.name}
+						<div className="w-0 group-hover:w-full h-1 bg-[#ff7d1aff] transition duration-1000 ease-in origin-left" />
+					</a>
+				))}
+			</div>
+			<div>
+				<LuShoppingCart />
+			</div>
 		</div>
 	);
 };
