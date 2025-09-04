d4497491c01105a557911c670019f1d6c31af02c
Eh1z <godsentehiz11@gmail.com>
2025-09-04 15:18:17 +0100

    technology page started

--- src/pages/Crew.tsx
+++ src/pages/Crew.tsx
@@ -29,13 +29,30 @@ const Crew = () => {
 	];
 
 	const [activeIndex, setActiveIndex] = useState(0);
+	const [bg, setBg] = useState(getBg());
+
+	function getBg() {
+		if (window.innerWidth < 640) {
+			return 'url("/crew/background-crew-mobile.jpg")';
+		} else if (window.innerWidth < 768) {
+			return 'url("/crew/background-crew-tablet.jpg")';
+		} else {
+			return 'url("/crew/background-crew-desktop.jpg")';
+		}
+	}
+
+	useEffect(() => {
+		const handleResize = () => {
+			setBg(getBg());
+		};
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
 
 	return (
 		<div
 			className="padding w-full min-h-screen bg-cover bg-center bg-no-repeat flex justify-center items-center"
-			style={{
-				backgroundImage: `url('/crew/background-crew-desktop.jpg')`,
-			}}
+			style={{ backgroundImage: bg }}
 		>
 			{/* Crew Carousel */}
 			<div className="content grid grid-cols-1 xl:grid-cols-2 gap-32  pt-16 text-white transition-all duration-700 ease-[cubic-bezier(0.87,0,0.13,1)]">
--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Technology = () => {
-	return <div>Technology</div>;
+	const [bg, setBg] = useState(getBg());
+
+	function getBg() {
+		if (window.innerWidth < 640) {
+			return 'url("/technology/background-technology-mobile.jpg")';
+		} else if (window.innerWidth < 768) {
+			return 'url("/technology/background-technology-tablet.jpg")';
+		} else {
+			return 'url("/technology/background-technology-desktop.jpg")';
+		}
+	}
+
+	useEffect(() => {
+		const handleResize = () => {
+			setBg(getBg());
+		};
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
+	return (
+		<div
+			className="padding w-full min-h-screen bg-cover bg-center bg-no-repeat flex justify-center items-center"
+			style={{ backgroundImage: bg }}
+		>
+			{" "}
+			tech
+		</div>
+	);
 };
 
 export default Technology;
